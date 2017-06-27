e8bcbaf611cf36045a74a9407672bdba46dd58e1
lgy <764448863@qq.com>
2017-06-27 17:46:29 +0800

    20170627 初完成版v001

--- pages/index/index.js
+++ pages/index/index.js
@@ -14,6 +14,9 @@ Page({
     imgUrls: [
       'http://img02.tooopen.com/images/20150928/tooopen_sy_143912755726.jpg',
       'http://img06.tooopen.com/images/20160818/tooopen_sy_175866434296.jpg',
+      'http://img06.tooopen.com/images/20160818/tooopen_sy_175833047715.jpg',
+      'http://img02.tooopen.com/images/20150928/tooopen_sy_143912755726.jpg',
+      'http://img06.tooopen.com/images/20160818/tooopen_sy_175866434296.jpg',
       'http://img06.tooopen.com/images/20160818/tooopen_sy_175833047715.jpg'
     ],//图片来源
     indicatorDots: true,//是否有点
